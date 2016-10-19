8e5be229f2a85486e2d3a4139dda80f197c2015f
Miles Smith <themilessmith@gmail.com>
2016-10-19 10:34:33 -0700

    Fixed Eric's fuck up

--- index.js
+++ index.js
@@ -54,7 +54,7 @@ app.post('/webhook/', function (req, res) {
 	    let event = req.body.entry[0].messaging[i]
 	    let sender = event.sender.id
 	    if (event.message && event.message.text){
-	        let text = getText(event.message.text)
+	        let text = getResponse(event.message.text)
             sendTextMessage(sender, text)
         }
     }
